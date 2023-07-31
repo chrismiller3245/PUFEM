d2d7b3f954ba9a11cc114e7e144d3a1cfd78ccfd
Christopher Miller <109816219+chrismiller3245@users.noreply.github.com>
2023-07-31 18:28:44 +0100

    Add files via upload

---
+++ FEAP/user/uplot.f
@@ -0,0 +1,25 @@
+c$Id: uplot.f,v 1.1 2007/08/15 22:42:22 rlt Exp $
+      subroutine uplot(ct)
+
+c      * * F E A P * * A Finite Element Analysis Program
+
+c....  Copyright (c) 1984-2007: Regents of the University of California
+c                               All rights reserved
+
+c-----[--.----+----.----+----.-----------------------------------------]
+c     Modification log                                Date (dd/mm/year)
+c       Original version                                    01/11/2006
+c-----[--.----+----.----+----.-----------------------------------------]
+c      Purpose:
+
+c      Inputs:
+
+c      Outputs:
+c-----[--.----+----.----+----.-----------------------------------------]
+      implicit  none
+
+      real*8    ct(3)
+
+      write(*,*) ' *WARNING* No user plot available.'
+
+      end
